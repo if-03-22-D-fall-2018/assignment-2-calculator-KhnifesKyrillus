4cc44f0e33f241096f8ec301b8945e5bc3eb54b7
Kyrillus <khnifes.kyrillusgmail.com>
2018-09-27 12:14:40 +0200

    is not working

--- calculator.c
+++ calculator.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
-  /* code */
-  return 0;
+int main(int argc, const char *argv[])
+{
+    iprintf("Choose one of the following operations:\n");
+    printf("\tAdd (1)\n");
+    printf("\tSubtract (2)\n");
+    printf("\tMultiply (3)\n");
+    printf("\tDivide (4)\n");
+    printf("\tStop program (-1)\n");
+    int input;
+    scanf("%d\n", &input);
+    switch (input)
+    {
+        case -1:
+          return 0;
+            break;
+        case 1:
+
+            break;
+        case 2:
+          Subtraction();
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case default:
+          printf("Input is not valid!!!\n");
+            break;
+    }
+
+    return 0;
+}
+int FirstOpertor()
+{
+  int firstOprator;
+
+  printf("Please enter the first operand: ", );
+  scanf("%d\n", &firstOprator );
+  return firstOprator;
+}
+int Subtraction(int firstOperator,int secondOperator)
+{
+
 }
