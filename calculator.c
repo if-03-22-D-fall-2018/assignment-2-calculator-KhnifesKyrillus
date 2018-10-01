8423ba02d138537d02efcafec00806ae081e01b3
KhnifesKyrillus <khnifes.kyrillus@gmail.com>
2018-10-01 18:20:42 +0200

    editet input and solved errors

--- calculator.c
+++ calculator.c
@@ -5,38 +5,40 @@ int GetSecondOperand();
 
 int main(int argc, const char *argv[])
 {
-  do {
+  int input;
+  do
+  {
     printf("Choose one of the following operations:\n");
     printf("Add (1)\n");
     printf("Subtract (2)\n");
     printf("Multiply (3)\n");
     printf("Divide (4)\n");
-    printf("Stop program (-1)\n");
-    int input;
-    scanf("%d\n", &input);
-    if (input<5 && input>=-1)
+    printf("Stop program (-1)\n\n");
+    scanf("%d", &input);
+    if (input>5 && input>=-1)
     {
       printf("Input is invalid!!!\n" );
     }
-  } while(input<5 && input>=-1);
+  }while(input<5 && input>=-1);
+  int firstOperand=GetFirstOperand();
+  int secondOperand=GetSecondOperand();
+  switch (input)
+  {
+    case -1:
+      return 0;
+    break;
+    case 1:
 
-    switch (input)
-    {
-        case -1:
-          return 0;
-            break;
-        case 1:
+    break;
+    case 2:
 
-            break;
-        case 2:
+    break;
+    case 3:
 
-            break;
-        case 3:
+    break;
+    case 4:
 
-            break;
-        case 4:
-
-            break;
+    break;
     }
 
     return 0;
